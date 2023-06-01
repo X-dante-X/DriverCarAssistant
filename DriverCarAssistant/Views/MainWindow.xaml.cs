87746db1296b426e84653ddcf86586f11c960023
X-dante-X <solomamd17@gmail.com>
2023-06-01 16:33:53 +0200

    Edit main style

--- DriverCarAssistant/Views/MainWindow.xaml.cs
+++ DriverCarAssistant/Views/MainWindow.xaml.cs
@@ -24,26 +24,48 @@ namespace DriverCarAssistant
         public MainWindow()
         {
             InitializeComponent();
+            ResetButtonState();
+            ((Button)FindName("Button1")).Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#1EBD55"));
+            ((Button)FindName("Button1")).Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#202020"));
             var page = new Drivers();
             mainFrame.Content = page;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            ResetButtonState();
+            ((Button)FindName("Button1")).Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#1EBD55"));
+            ((Button)FindName("Button1")).Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#202020"));
             var page = new Drivers();
             mainFrame.Content = page;
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            ResetButtonState();
+            ((Button)FindName("Button2")).Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#1EBD55"));
+            ((Button)FindName("Button2")).Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#202020"));
             var page = new Cars();
             mainFrame.Content = page;
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
+            ResetButtonState();
+            ((Button)FindName("Button3")).Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#1EBD55"));
+            ((Button)FindName("Button3")).Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#202020"));
             var page = new Trailers();
             mainFrame.Content = page;
+        }
+
+        private void ResetButtonState()
+        {
+            ((Button)FindName("Button1")).Foreground = Brushes.White;
+            ((Button)FindName("Button2")).Foreground = Brushes.White;
+            ((Button)FindName("Button3")).Foreground = Brushes.White;
+            ((Button)FindName("Button1")).Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#30302F"));
+            ((Button)FindName("Button2")).Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#30302F"));
+            ((Button)FindName("Button3")).Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#30302F"));
         }
     }
 }
